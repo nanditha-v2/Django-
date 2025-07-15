e83bb357d46d76666c09427e838bdcd72907f28b
Pranav P <pranav.p.nair@xminds.com>
2025-07-15 15:36:29 +0530

    (Initial commit)

--- api/serializer.py
+++ api/serializer.py
@@ -1,17 +1,21 @@
 from rest_framework import serializers
 from .models import User, UserProfile
 
-
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = '__all__' 
-
+        fields = ['phone', 'address', 'birth_date']
 
 class UserSerializer(serializers.ModelSerializer):
-     profile = UserProfileSerializer(read_only=True)
+    profile = UserProfileSerializer()
 
-     class Meta:
+    class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'age', 'name', 'profile']
 
+    def create(self, validated_data):
+        profile_data = validated_data.pop('profile', None)
+        user = User.objects.create(**validated_data)
+        if profile_data:
+            UserProfile.objects.create(user=user, **profile_data)
+        return user
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import get_user, CREATE_user
 
+
 urlpatterns = [
     path('users/', get_user, name='get_user'),
     path('users/create/', CREATE_user, name='create_user')
+   
 ]
